d353801ff5401ebf298fb710b340111384ab57b3
Kathleen Jung <ovo0325@outlook.kr>
2022-11-17 17:22:56 +0900

    Sign-In UI modify
    Former-commit-id: eecf18541cc9fbe902a5053d92d0eccb881fb76d

--- src/components/user/UserLogin.vue
+++ src/components/user/UserLogin.vue
@@ -8,30 +8,32 @@
     <b-row>
       <b-col></b-col>
       <b-col cols="8">
-        <b-card class="text-center mt-3" style="max-width: 40rem" align="left">
-          <b-form class="text-left">
+        <b-card class="text-center mt-3" style="max-width: 40rem; background-color: #fafafa;" align="center">
+          <div>Sign in with</div>
+          <b-button type="button" variant="success" class="m-1" @click="movePage">회원가입</b-button>
+          <div>Or sign in with credentials</div>
+          <b-form class="m-5">
             <b-alert show variant="danger" v-if="isLoginError">아이디 또는 비밀번호를 확인하세요.</b-alert>
-            <b-form-group label="아이디:" label-for="userid">
+            <b-form-group label-for="userid">
               <b-form-input
                 id="userid"
                 v-model="user.userid"
                 required
-                placeholder="아이디 입력...."
+                placeholder="아이디"
                 @keyup.enter="confirm"
               ></b-form-input>
             </b-form-group>
-            <b-form-group label="비밀번호:" label-for="userpwd">
+            <b-form-group label-for="userpwd">
               <b-form-input
                 type="password"
                 id="userpwd"
                 v-model="user.userpwd"
                 required
-                placeholder="비밀번호 입력...."
+                placeholder="비밀번호"
                 @keyup.enter="confirm"
               ></b-form-input>
             </b-form-group>
             <b-button type="button" variant="primary" class="m-1" @click="confirm">로그인</b-button>
-            <b-button type="button" variant="success" class="m-1" @click="movePage">회원가입</b-button>
           </b-form>
         </b-card>
       </b-col>
