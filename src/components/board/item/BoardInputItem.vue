5d1af8d38218b17f77fed011cf73b056f486cd91
Kathleen Jung <ovo0325@outlook.kr>
2022-11-17 16:20:01 +0900

    Board 기능 구현
    Former-commit-id: 847eb01167cd88e800037d024bb5c73090ca8f38

--- src/components/board/item/BoardInputItem.vue
+++ src/components/board/item/BoardInputItem.vue
@@ -6,7 +6,7 @@
           <b-form-input
             id="userid"
             :disabled="isUserid"
-            v-model="article.userid"
+            v-model="article.userId"
             type="text"
             required
             placeholder="작성자 입력..."
@@ -35,7 +35,7 @@
 
         <b-button type="submit" variant="primary" class="m-1" v-if="this.type === 'register'">글작성</b-button>
         <b-button type="submit" variant="primary" class="m-1" v-else>글수정</b-button>
-        <b-button type="reset" variant="danger" class="m-1">초기화</b-button>
+        <b-button type="reset" variant="danger" class="m-1">취소</b-button>
       </b-form>
     </b-col>
   </b-row>
@@ -62,7 +62,7 @@ export default {
   },
   created() {
     if (this.type === "modify") {
-      http.get(`/board/${this.$route.params.articleno}`).then(({ data }) => {
+      http.get(`/board/view/${this.$route.params.articleno}`).then(({ data }) => {
         // this.article.articleno = data.article.articleno;
         // this.article.userid = data.article.userid;
         // this.article.subject = data.article.subject;
@@ -78,8 +78,9 @@ export default {
 
       let err = true;
       let msg = "";
-      !this.article.userid && ((msg = "작성자 입력해주세요"), (err = false), this.$refs.userid.focus());
-      err && !this.article.subject && ((msg = "제목 입력해주세요"), (err = false), this.$refs.subject.focus());
+      // !this.article.userid && ((msg = "작성자 입력해주세요"), (err = false), this.$refs.userid.focus());
+      // err &&
+      !this.article.subject && ((msg = "제목 입력해주세요"), (err = false), this.$refs.subject.focus());
       err && !this.article.content && ((msg = "내용 입력해주세요"), (err = false), this.$refs.content.focus());
 
       if (!err) alert(msg);
@@ -93,41 +94,52 @@ export default {
       this.moveList();
     },
     registArticle() {
+      console.log(this.article.userId);
+      console.log(this.article.userid);
       http
-        .post(`/board`, {
-          userid: this.article.userid,
+        .post(`/board/write`, {
+          userId: this.article.userId,
           subject: this.article.subject,
           content: this.article.content,
         })
         .then(({ data }) => {
-          let msg = "등록 처리시 문제가 발생했습니다.";
-          if (data === "success") {
-            msg = "등록이 완료되었습니다.";
-          }
-          alert(msg);
+          console.log(data);
+          // let msg = "등록 처리시 문제가 발생했습니다.";
+          // if (data === "success") {
+          //   msg = "등록이 완료되었습니다.";
+          // }
+          // alert(msg);
           this.moveList();
         });
     },
     modifyArticle() {
       http
-        .put(`/board`, {
-          articleno: this.article.articleno,
-          userid: this.article.userid,
+        .put(`/board/modify/${this.article.articleNo}`, {
+          articleNo: this.article.articleNo,
+          userId: this.article.userId,
           subject: this.article.subject,
           content: this.article.content,
         })
         .then(({ data }) => {
-          let msg = "수정 처리시 문제가 발생했습니다.";
-          if (data === "success") {
-            msg = "수정이 완료되었습니다.";
-          }
-          alert(msg);
+          console.log(data);
+          // let msg = "수정 처리시 문제가 발생했습니다.";
+          // if (data === "success") {
+          //   msg = "수정이 완료되었습니다.";
+          // }
+          // alert(msg);
           // 현재 route를 /list로 변경.
-          this.moveList();
+          this.viewArticle(data);
         });
     },
     moveList() {
       this.$router.push({ name: "boardlist" });
+    },
+    
+    viewArticle(article) {
+      this.$router.push({
+        name: "boardview",
+        params: { articleno: article.articleNo },
+      });
     },
   },
 };
