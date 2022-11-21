83daeb882e6cee10a29b434fd76ef0562f06a8e2
Kathleen Jung <ovo0325@outlook.kr>
2022-11-21 10:18:46 +0900

    ui
    Former-commit-id: b954574553655a0bdd40ae7bbcda57505a8a4777

---
+++ src/components/house/HouseLeft.vue
@@ -0,0 +1,66 @@
+<template>
+  <div style="max-width: 80%">
+    <div class="m-5">
+      <b>동으로 검색</b>
+      <b-form-group id="input-sido">
+        <b-form-select
+          id="input-sido"
+          v-model="form.food"
+          :options="foods1"
+          required
+        ></b-form-select>
+      </b-form-group>
+      <b-form-group id="input-gugun">
+        <b-form-select
+          id="input-gugun"
+          v-model="form.food"
+          :options="foods2"
+          required
+        ></b-form-select>
+      </b-form-group>
+      <b-form-group id="input-dong">
+        <b-form-select
+          id="input-dong"
+          v-model="form.food"
+          :options="foods3"
+          required
+        ></b-form-select>
+      </b-form-group>
+      <b-button variant="outline-primary">검색</b-button>
+    </div>
+    <div class="m-5">
+      <b>아파트로 검색</b>
+      <b-form-group id="input-group-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          placeholder="아파트명"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-button variant="outline-primary">검색</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HouseLeft",
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+        checked: [],
+        food: null,
+      },
+      foods1: [{ text: "시", value: null }, "Carrots", "Beans", "Tomatoes", "Corn"],
+      foods2: [{ text: "군", value: null }, "Carrots", "Beans", "Tomatoes", "Corn"],
+      foods3: [{ text: "구", value: null }, "Carrots", "Beans", "Tomatoes", "Corn"],
+      show: true,
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/house/HouseRight.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="max-width: 80%">
+    <b-img :src="require('@/assets/happyhouse.png')" style="width: 400px"></b-img>
+    <div id="aptName">아파트 이름</div>
+    <div id="text">지번 주소</div>
+    <div id="text">도로명 주소</div>
+    <div id="text">건축년도</div>
+    <div id="text">매매 정보</div>
+    <div id="text">통계 정보</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#text {
+  font-family: "NanumSquare";
+}
+#aptName {
+  font-family: "NanumSquareExtraBold";
+  font-size: xx-large;
+  text-align: left;
+  margin: 10px;
+}
+</style>
