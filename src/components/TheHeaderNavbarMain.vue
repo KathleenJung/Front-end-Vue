ba0db5427e305c94931205ea865f786cafc172b4
홍예진 <teepij@naver.com>
2022-11-21 15:59:43 +0900

    impl logout server(delete token)

--- src/components/TheHeaderNavbarMain.vue
+++ src/components/TheHeaderNavbarMain.vue
@@ -95,11 +95,11 @@ export default {
       // this.SET_USER_INFO(null);
       // sessionStorage.removeItem("access-token");
       // if (this.$route.path != "/") this.$router.push({ name: "main" });
-      console.log(this.userInfo.userid);
+      console.log(this.userInfo.userId);
       //vuex actions에서 userLogout 실행(Backend에 저장 된 리프레시 토큰 없애기
       //+ satate에 isLogin, userInfo 정보 변경)
       // this.$store.dispatch("userLogout", this.userInfo.userid);
-      this.userLogout(this.userInfo.userid);
+      this.userLogout(this.userInfo.userId);
       sessionStorage.removeItem("accessToken"); //저장된 토큰 없애기
       sessionStorage.removeItem("refreshToken"); //저장된 토큰 없애기
       if (this.$route.path != "/") this.$router.push({ name: "main" });
